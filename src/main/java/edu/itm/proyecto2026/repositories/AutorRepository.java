1afa96631d59251f2c8fe4a9406030db447804d5
DilanJaramilloR-dev <dilanjaramillo1152233@correo.itm.edu.co>
2026-09-22 20:54:56 -0500

    ahora si conexion

--- src/main/java/edu/itm/proyecto2026/repositories/AutorRepository.java
+++ src/main/java/edu/itm/proyecto2026/repositories/AutorRepository.java
@@ -1,38 +1,86 @@
 package edu.itm.proyecto2026.repositories;
 
 import edu.itm.proyecto2026.identities.Autor;
+import edu.itm.proyecto2026.services.utilities.Conexion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.time.LocalDate;
 
 //nuevos imports
 import org.springframework.jdbc.core.JdbcTemplate;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
 @Repository
 public class AutorRepository {
 
-    private final JdbcTemplate  jdbcTemplate;
-    public AutorRepository(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
+    @Autowired
+    private AutoresDAOHelper helper;
 
     public List<Autor> getAutor() {
-        // List<Autor> autor = new ArrayList<>();
-                String sql = "SELECT id_autor, nombre_autor, apellido_autor, nacionalidad_autor, fecha_nacimiento FROM autor";
-                return jdbcTemplate.query(sql, this::mapRowToAutor);
+
+        List<Autor> autor = new ArrayList<>();
+
+        Conexion conexion = new Conexion();
+        Connection con = conexion.obtenerConexion();
+
+
+        try {
+            PreparedStatement ps = con.prepareStatement(helper.listarAutores());
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                Autor autor1 = Autor.builder()
+                        .idAutor(rs.getLong("id_autor"))
+                        .nombreAutor(rs.getString("nombre_autor"))
+                        .apellidoAutor(rs.getString("apellido_autor"))
+                        .nacionalidadAutor(rs.getString("nacionalidad_autor"))
+                        .fechaNacimiento(rs.getDate("fecha_nacimiento").toLocalDate())
+                        .build();
+                autor.add(autor1);
+            }
+        }catch (SQLException sqlException){
+            sqlException.printStackTrace();
+
+        }catch (Exception exception){
+            exception.printStackTrace();
+        }finally {
+            try {
+                con.close();
+            }catch (SQLException sqlException){
+                sqlException.printStackTrace();
+            }
+        }
+        return autor;
     }
 
-    private Autor mapRowToAutor(ResultSet rs, int rowNum) throws SQLException {
-    return Autor.builder()
-            .idAutor(rs.getLong("id_autor"))
-            .nombreAutor(rs.getString("nombre_autor"))
-            .apellidoAutor(rs.getString("apellido_autor"))
-            .nacionalidadAutor(rs.getString("nacionalidad_autor"))
-            .fechaNacimiento(rs.getDate("fecha_nacimiento").toLocalDate())
-            .build();
-}
+
+    public Autor insertarAutor(Autor autor){
+        Conexion conexion = new Conexion();
+        Connection con = conexion.obtenerConexion();
+
+        try {
+            PreparedStatement ps = con.prepareStatement(helper.insertarAutor());
+            ps.setLong(1, autor.getIdAutor());
+            ps.setString(2, autor.getNombreAutor());
+            ps.setString(3, autor.getApellidoAutor());
+            ps.setString(4, autor.getNacionalidadAutor());
+            ps.setDate(5, Date.valueOf(autor.getFechaNacimiento()));
+            ps.executeUpdate();
+
+        }catch (Exception exception){
+            exception.printStackTrace();
+            autor = null;
+        }finally {
+            try {
+                con.close();
+            }catch (SQLException sqlException){
+                sqlException.printStackTrace();
+            }
+        }
+        return  autor;
+    }
+
+             /*   String sql = "SELECT id_autor, nombre_autor, apellido_autor, nacionalidad_autor, fecha_nacimiento FROM autor";*/
 }
---
+++ src/main/java/edu/itm/proyecto2026/repositories/AutoresDAOHelper.java
@@ -0,0 +1,14 @@
+package edu.itm.proyecto2026.repositories;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AutoresDAOHelper {
+    public String listarAutores(){
+        return "Select id_autor, nombre_autor, apellido_autor, nacionalidad_autor, fecha_nacimiento from autor";
+    }
+
+    public String insertarAutor(){
+        return  "Insert into autor (id_autor, nombre_autor, apellido_autor, nacionalidad_autor, fecha_nacimiento) values (?,?,?,?,?)";
+    }
+}
